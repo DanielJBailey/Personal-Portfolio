2a2edba50f491b76f207c38722076d6e79dad44e
DanielJBailey <Dbailey0210@gmail.com>
2018-08-22 23:12:34 -0500

    remove doc ready

--- build/js/index.js
+++ build/js/index.js
@@ -1,42 +1,33 @@
-document.body.addEventListener('DOMContentLoaded', function() {
-
-    let scroll = document.querySelector('.back-to-top');
-    scroll.addEventListener('click', function () {
-        window.scroll({ top: 0, left: 0, behavior: 'smooth' }); 
+// get skill section container and store it in variable
+let skillsSection = document.querySelector('.skills');
+// remove grayscale filter class when mouse enters
+skillsSection.addEventListener('mouseenter', function () {
+    let icons = document.querySelectorAll('.tech-icon');
+    icons.forEach((icon) => {
+        icon.classList.remove('filtered');
     });
-
-    // get skill section container and store it in variable
-    let skillsSection = document.querySelector('.skills');
-    // remove grayscale filter class when mouse enters
-    skillsSection.addEventListener('mouseenter', function () {
-        let icons = document.querySelectorAll('.tech-icon');
-        icons.forEach((icon) => {
-            icon.classList.remove('filtered');
-        });
+});
+// add grayscale filter class when mouse leaves
+skillsSection.addEventListener('mouseleave', function () {
+    let icons = document.querySelectorAll('.tech-icon');
+    icons.forEach((icon) => {
+        icon.classList.add('filtered');
     });
-    // add grayscale filter class when mouse leaves
-    skillsSection.addEventListener('mouseleave', function () {
-        let icons = document.querySelectorAll('.tech-icon');
-        icons.forEach((icon) => {
-            icon.classList.add('filtered');
-        });
-    });
+});
 
-    //select all projects and add event listener
-    let projects = document.querySelectorAll('.project');
-    projects.forEach((project) => {
-    //remove filtered class on hover
-        project.addEventListener('mouseenter', function () {
-            let children = this.children;
-            children[0].classList.remove('hidden');
-        });
-        //add filtered class on mouse leave
-        project.addEventListener('mouseleave', function () {
-            let children = this.children;
-            children[0].classList.add('hidden');
-        });
+//select all projects and add event listener
+let projects = document.querySelectorAll('.project');
+projects.forEach((project) => {
+//remove filtered class on hover
+    project.addEventListener('mouseenter', function () {
+        let children = this.children;
+        children[0].classList.remove('hidden');
     });
-
-})
+    //add filtered class on mouse leave
+    project.addEventListener('mouseleave', function () {
+        let children = this.children;
+        children[0].classList.add('hidden');
+    });
+});
 
 
--- build/js/script.js
+++ build/js/script.js
@@ -1,41 +1,33 @@
 'use strict';
 
-document.body.addEventListener('DOMContentLoaded', function () {
-
-    var scroll = document.querySelector('.back-to-top');
-    scroll.addEventListener('click', function () {
-        window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+// get skill section container and store it in variable
+var skillsSection = document.querySelector('.skills');
+// remove grayscale filter class when mouse enters
+skillsSection.addEventListener('mouseenter', function () {
+    var icons = document.querySelectorAll('.tech-icon');
+    icons.forEach(function (icon) {
+        icon.classList.remove('filtered');
     });
-
-    // get skill section container and store it in variable
-    var skillsSection = document.querySelector('.skills');
-    // remove grayscale filter class when mouse enters
-    skillsSection.addEventListener('mouseenter', function () {
-        var icons = document.querySelectorAll('.tech-icon');
-        icons.forEach(function (icon) {
-            icon.classList.remove('filtered');
-        });
+});
+// add grayscale filter class when mouse leaves
+skillsSection.addEventListener('mouseleave', function () {
+    var icons = document.querySelectorAll('.tech-icon');
+    icons.forEach(function (icon) {
+        icon.classList.add('filtered');
     });
-    // add grayscale filter class when mouse leaves
-    skillsSection.addEventListener('mouseleave', function () {
-        var icons = document.querySelectorAll('.tech-icon');
-        icons.forEach(function (icon) {
-            icon.classList.add('filtered');
-        });
-    });
+});
 
-    //select all projects and add event listener
-    var projects = document.querySelectorAll('.project');
-    projects.forEach(function (project) {
-        //remove filtered class on hover
-        project.addEventListener('mouseenter', function () {
-            var children = this.children;
-            children[0].classList.remove('hidden');
-        });
-        //add filtered class on mouse leave
-        project.addEventListener('mouseleave', function () {
-            var children = this.children;
-            children[0].classList.add('hidden');
-        });
+//select all projects and add event listener
+var projects = document.querySelectorAll('.project');
+projects.forEach(function (project) {
+    //remove filtered class on hover
+    project.addEventListener('mouseenter', function () {
+        var children = this.children;
+        children[0].classList.remove('hidden');
+    });
+    //add filtered class on mouse leave
+    project.addEventListener('mouseleave', function () {
+        var children = this.children;
+        children[0].classList.add('hidden');
     });
 });
